9ea49ec30823096089b926cc264b26f4917831bd
Michael Carrein <michael.carrein@hotmail.com>
2024-09-01 14:54:49 +0200

    Codacy fixes

--- public_html/web/js/consoleText.js
+++ public_html/web/js/consoleText.js
@@ -1,20 +1,20 @@
 // All credits and rights to Tobias.
 // https://codepen.io/Tbgse/pen/dYaJyJ
 function consoleText(words, id, colors) {
-  if (colors === undefined) colors = ['#fff'];
+  if (colors === undefined) colors = ["#fff"];
   var visible = true;
-  var con = document.getElementById('console');
+  var con = document.getElementById("console");
   var letterCount = 1;
   var x = 1;
   var waiting = false;
-  var target = document.getElementById(id)
-  target.setAttribute('style', 'color:' + colors[0])
-  window.setInterval(function() {
+  var target = document.getElementById(id);
+  target.setAttribute("style", "color:" + colors[0]);
+  window.setInterval(function () {
 
     if (letterCount === 0 && waiting === false) {
       waiting = true;
-      target.innerHTML = words[0].substring(0, letterCount)
-      window.setTimeout(function() {
+      target.innerHTML = words[0].substring(0, letterCount);
+      window.setTimeout(function () {
         var usedColor = colors.shift();
         colors.push(usedColor);
         var usedWord = words.shift();
@@ -23,7 +23,7 @@ function consoleText(words, id, colors) {
         target.setAttribute('style', 'color:' + colors[0])
         letterCount += x;
         waiting = false;
-      }, 1000)
+      }, 1000);
     } else if (letterCount === words[0].length + 1 && waiting === false) {
       waiting = true;
       window.setTimeout(function() {
@@ -35,16 +35,16 @@ function consoleText(words, id, colors) {
       target.innerHTML = words[0].substring(0, letterCount)
       letterCount += x;
     }
-  }, 120)
+  }, 120);
   window.setInterval(function() {
     if (visible === true) {
-      con.className = 'console-underscore hidden'
+      con.className = "console-underscore hidden";
       visible = false;
 
     } else {
-      con.className = 'console-underscore'
+      con.className = "console-underscore";
 
       visible = true;
     }
-  }, 400)
+  }, 400);
 }
