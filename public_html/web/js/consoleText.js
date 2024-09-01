3a78deb2d3fedf733f5efe48521a2d8c1aae0897
Michael <michael.carrein@hotmail.com>
2024-09-01 15:34:29 +0200

    Update consoleText.js

--- public_html/web/js/consoleText.js
+++ public_html/web/js/consoleText.js
@@ -20,23 +20,23 @@ function consoleText(words, id, colors) {
         var usedWord = words.shift();
         words.push(usedWord);
         x = 1;
-        target.setAttribute('style', 'color:' + colors[0])
+        target.setAttribute("style", "color:" + colors[0]);
         letterCount += x;
         waiting = false;
       }, 1000);
     } else if (letterCount === words[0].length + 1 && waiting === false) {
       waiting = true;
-      window.setTimeout(function() {
+      window.setTimeout(function () {
         x = -1;
         letterCount += x;
         waiting = false;
       }, 1000)
     } else if (waiting === false) {
-      target.innerHTML = words[0].substring(0, letterCount)
+      target.innerHTML = words[0].substring(0, letterCount);
       letterCount += x;
     }
   }, 120);
-  window.setInterval(function() {
+  window.setInterval(function () {
     if (visible === true) {
       con.className = "console-underscore hidden";
       visible = false;
