85799a04aa38653c26d829cc2d99201637b10afa
Michael Carrein <michael.carrein@hotmail.com>
2024-09-15 16:15:22 +0200

    Fix console bug unused argv

--- public_html/app/console.php
+++ public_html/app/console.php
@@ -11,19 +11,22 @@ $migrationManager = new MigrationManager();
 // Add migrations here
 $migrationManager->addMigration(new \app\Migration\CreateTOTPEmail($dbh));
 
-$allowedArgs = ['--migrate', '--rollback'];
-if (in_array($argv[1], $allowedArgs) === false) {
+$allowedArgs = ['--migrate', '--rollback', '-m', '-r'];
+if ((bool) isset($argv[1]) === false || in_array($argv[1], $allowedArgs) === false) {
     print_r("Invalid command. Use '-m = --migrate' or '-r = --rollback'." . PHP_EOL);
 }
 
-$mArgs = ['-m', $allowedArgs[0]];
-if (in_array($argv[1], $mArgs) === true) {
-    $migrationManager->migrate();
-    print_r("Migrations applied successfully." . PHP_EOL);
-}
+if((bool) isset($argv[1]) === true)
+{
+    $mArgs = ['-m', $allowedArgs[0]];
+    if (in_array($argv[1], $mArgs) === true) {
+        $migrationManager->migrate();
+        print_r("Migrations applied successfully." . PHP_EOL);
+    }
 
-$rArgs = ['-r', $allowedArgs[1]];
-if (in_array($argv[1], $rArgs) === true) {
-    $migrationManager->rollback();
-    print_r("Migrations rolled back successfully." . PHP_EOL);
+    $rArgs = ['-r', $allowedArgs[1]];
+    if (in_array($argv[1], $rArgs) === true) {
+        $migrationManager->rollback();
+        print_r("Migrations rolled back successfully." . PHP_EOL);
+    }
 }
