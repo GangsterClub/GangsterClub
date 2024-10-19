395a71107a57550d547ebad764dbb58fe0bbfb4e
Michael Carrein <michael.carrein@hotmail.com>
2024-10-19 10:33:50 +0200

    Add OTP handlePaste

--- public_html/web/js/otp-input.js
+++ public_html/web/js/otp-input.js
@@ -14,12 +14,29 @@ document.addEventListener("DOMContentLoaded", (event) => {
         }
     }
 
+    // Handling pasted OTP of 6 digits
+    const handlePaste = (evt) => {
+        evt.preventDefault();
+        const pastedText = evt.clipboardData.getData('text').trim();
+        if (!/^[0-9]{6}$/.test(pastedText)) { // Change {6} if needed.
+            return;
+        }
+        const digits = pastedText.split('');
+        let inputs = document.querySelectorAll('[data-focus-input-init]');
+        inputs.forEach((input, index) => input.value = digits[index]);
+        inputs[inputs.length - 1].focus();
+    };
+
+    // Apply listeners and "hack" value afterwards
     document.querySelectorAll('[data-focus-input-init]').forEach(function(element) {
+        element.addEventListener('paste', handlePaste);
         element.addEventListener('keyup', function() {
             const prevId = this.getAttribute('data-focus-input-prev');
             const nextId = this.getAttribute('data-focus-input-next');
             focusNextInput(this, prevId, nextId);
         });
+
+        // Hacking the values after applying the listeners makes focusNextInput() work as intended.
         element.value = element.getAttribute('placeholder');
     });
 });
