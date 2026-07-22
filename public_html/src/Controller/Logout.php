3e2efa48b3e69e38da1cb1a0e8d19f53bbfef966
Michael <michael.carrein@hotmail.com>
2026-07-22 19:31:29 +0200

    Handle safe logout requests gracefully

--- public_html/src/Controller/Logout.php
+++ public_html/src/Controller/Logout.php
@@ -12,7 +12,12 @@ class Logout extends Controller
     public function __invoke(Request $request): Response
     {
         $auth = $this->auth();
+        if ($request->getMethod() !== 'POST') {
+            $redirect = $auth->getAuthenticatedUserId() === null ? '/login' : '/account';
+            return Response::redirect(APP_BASE . $redirect, 303);
+        }
+
         $auth->logoutUser();
-        return Response::redirect(APP_BASE . '/login', 301);
+        return Response::redirect(APP_BASE . '/login', 303);
     }
 }
